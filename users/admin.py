70785d4d99952d30755146c0a7a1df39edb59639
Ashrafulx <ashrafulwho@gmail.com>
2026-09-26 21:12:11 +0600

    queryfix

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from users.models import CustomUser
-# Register your models here.
-admin.site.register(CustomUser)
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "user_permissions":
+            kwargs["queryset"] = (
+                db_field.remote_field.model.objects
+                .select_related("content_type")
+            )
+
+        return super().formfield_for_manytomany(
+            db_field,
+            request,
+            **kwargs
+        )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
